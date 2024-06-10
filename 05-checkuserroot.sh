22e7c79ce2e96f3296d56475930c7ea109534548
ArpanaM <muriki.arpana@gmail.com>
2024-06-10 16:40:19 +0530

    check user is root user

---
+++ 05-checkuserroot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#### Check whether the user is running as a root user or not
+
+USER_ID=$(id -u)
+echo USER_ID
+
+if [ "${USER_ID}" -ne 0 ]; then
+
+  echo -e "\e[1 ;31mYou must be a root user to perform this script\e[0m"
+  exit
+  else
+    yum install nginx -y
+
+fi
