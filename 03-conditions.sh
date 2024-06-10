8a4287b955625e3a734f3a1ba275a6a2b705c5cb
ArpanaM <muriki.arpana@gmail.com>
2024-06-10 15:48:33 +0530

    condtions

--- 03-conditions.sh
+++ 03-conditions.sh
@@ -3,6 +3,8 @@
 read -p 'Enter your age--' age
 if [ "${age}" -lt 18 ]; then
   echo you are minor
+  elif [ "${age}" -gt 60 ]; then
+    echo you are senior citizen
   else
     echo you are major
     fi
