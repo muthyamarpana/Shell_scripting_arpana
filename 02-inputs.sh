55ab0b23c3818a17f3b2df2613897aaafecd7c17
ArpanaM <muriki.arpana@gmail.com>
2024-06-10 15:37:07 +0530

    inputs before execution

--- 02-inputs.sh
+++ 02-inputs.sh
@@ -4,3 +4,11 @@ read -p 'enter your name:' name
 read -p 'enter your age:' age
 
 echo "your name-${name}, Your age-${age}"
+
+## inputs before Execution
+
+echo ${0}
+echo ${1}
+echo ${2}
+echo ${*}
+echo ${#}
