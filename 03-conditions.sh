201774a726200cca91475be941af66bfce720757
ArpanaM <muriki.arpana@gmail.com>
2024-06-10 15:50:52 +0530

    condtions

--- 03-conditions.sh
+++ 03-conditions.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 read -p 'Enter your age--' age
+
+if [ -z "$age" ]; then
+    echo input is missing
+    exit
+fi
 if [ "${age}" -lt 18 ]; then
   echo you are minor
   elif [ "${age}" -gt 60 ]; then
