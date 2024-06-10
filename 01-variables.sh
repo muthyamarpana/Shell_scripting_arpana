960873d77e6fbea5a9725cb106d6eaea97275971
ArpanaM <muriki.arpana@gmail.com>
2024-06-10 15:21:18 +0530

    Array

--- 01-variables.sh
+++ 01-variables.sh
@@ -6,3 +6,15 @@ echo a in currency=${a}USD
 
 DATE=2021-12-08
 echo good morning, welcome , todays date is ${DATE}
+
+DATE=$(date +%F)
+
+ADD=$((2+3))
+echo Add=${ADD}
+
+x=(10 20 30 40 50)
+echo ${x[0]}
+echo ${x[1]}
+echo ${x[2]}
+echo ${x[3]}
+echo ${x[4]}
